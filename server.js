ab9a39615be7a5f2468d57af880468cb61428246
Sameer Khan <itsmdsameerkhan@gmail.com>
2021-01-15 15:13:37 +0545

    client url fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(require('cors')({}))
+app.use(require('cors')({ origin: process.env.CLIENT_URL }))
 // origin: process.env.CLIENT_URL
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config()
